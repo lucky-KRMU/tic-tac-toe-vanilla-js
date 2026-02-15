383cdf26d089035ac78b658416a5f664c4816838
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-15 04:30:22 +0000

    add win check logic

--- logic.js
+++ logic.js
@@ -4,6 +4,31 @@ let playerChecker = true; // for alternating X and O
 let dataArray = Array(9).fill(null);
 
 
+const winCheck = () =>{
+    let winCheckPara = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]; // all the winning possiblities
+
+    for (let i=0; i<winCheckPara.length; i++){
+        const [a, b, c] = winCheckPara[i];
+        if (dataArray[a] && dataArray[a] == dataArray[b] && dataArray[a] == dataArray[c]){
+            return dataArray[a];
+        }
+
+    }
+    return null;
+
+}
+
+
+
 console.log(buttons);
 
 buttons.forEach(btn=>{
@@ -15,9 +40,11 @@ buttons.forEach(btn=>{
                 e.target.innerText = "O"
             }
             playerChecker = !playerChecker;
+            let id = e.target.id;
+            dataArray[id] = e.target.innerText;
+            console.log(dataArray);
+            let check = winCheck();
+            console.log(check);
         }
-        let id = e.target.id;
-        dataArray[id] = e.target.innerText;
-        console.log(dataArray);
     })
 })
